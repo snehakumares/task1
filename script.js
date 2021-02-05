31adb92846fab6d4748da8574508d1be8d874f23
Sneha <snehakumares@gmail.com>
2021-02-05 22:48:23 +0530

    all

--- script.js
+++ script.js
@@ -16,3 +16,32 @@ function close() {
     var parent = this.parent;
     parent.style.display = "none";
 }
+
+function usersignin() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    if (email.value != "")
+        if (password.value != "") {
+            var modal = document.getElementById("signin-modal");
+            modal.style.display = "none";
+            var user = document.getElementById("user");
+            user.style.display = "block";
+            document.getElementById("name").innerHTML = email;
+            var option = document.getElementById("create-account");
+            option.style.display = "none";
+        }
+}
+
+function mobcreateaccount() {
+    var modal = document.getElementById("create-account-phone");
+    modal.style.display = "table";
+    var signin = document.getElementById("signin-phone");
+    signin.style.display = "none";
+}
+
+function signinphone() {
+    var createaccount = document.getElementById("create-account-phone");
+    createaccount.style.display = "none";
+    var modal = document.getElementById("signin-phone");
+    modal.style.display = "table";
+}
